5731dde644b88108b9437065eb9b5c7433b68eed
gulbalasalamov <g.salamov@gmail.com>
2021-09-05 08:28:08 +0300

    ExpressionClosure.java : Using DoubleUnaryOperator lambda expression that calculates the formula added

---
+++ lambdaexpressions/src/main/java/practice/ExpressionClosure.java
@@ -0,0 +1,19 @@
+package practice;
+
+import java.util.function.DoubleUnaryOperator;
+
+/**
+ * Using closure write a lambda expression that calculates a∗x2+b∗x+c where a, b, c are context final variables.
+ * They will be available in the context during testing. Note, the result is double.
+ * Solution format. Submit your lambda expression in any valid format with ; on the end.
+ * Examples: (x, y) -> x + y; (x, y) -> { return x + y; }
+ * https://hyperskill.org/learn/step/2367
+ */
+public class ExpressionClosure {
+    public static int a = 10;
+    public static int b = 20;
+    public static int c = 30;
+
+    public static DoubleUnaryOperator unaryOperator = // Write your code here
+            num -> a * num * num + b * num + c;
+}
