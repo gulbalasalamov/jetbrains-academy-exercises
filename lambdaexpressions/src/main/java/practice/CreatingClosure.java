50b208db2c84423955a85d38ebbe4024be072f9e
gulbalasalamov <g.salamov@gmail.com>
2021-09-05 08:23:46 +0300

    CreatingClosure.java : Using UnaryOperator , PrefixSuffixOperator

---
+++ lambdaexpressions/src/main/java/practice/CreatingClosure.java
@@ -0,0 +1,18 @@
+package practice;
+
+import java.util.function.Function;
+import java.util.function.UnaryOperator;
+
+/**
+ * Write a lambda expression that adds PREFIX (before) and SUFFIX (after) to its single string argument;
+ * PREFIX and SUFFIX are final variables. They are available in the code template.
+ * All whitespaces on the both ends of the argument must be removed. Do not trim prefix, suffix and the result string.
+ * https://hyperskill.org/learn/step/2368
+ */
+public class CreatingClosure {
+    public static final String PREFIX = "__pref__";
+    public static final String SUFFIX = "__suff__";
+
+    public static UnaryOperator<String> operator = text -> PREFIX + text.trim() + SUFFIX;
+    Function<String, String> fun = text -> PREFIX + text.trim() + SUFFIX;
+}
