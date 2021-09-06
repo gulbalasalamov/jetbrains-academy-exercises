dc6929bb64b71ce5d7910377a24192634ba3017b
gulbalasalamov <g.salamov@gmail.com>
2021-09-06 19:06:59 +0300

    PrintElementsOfStream.java: Arrays stream, map and skip methods to solve the problem.

---
+++ functionalprogramming/src/main/java/functionalstreams/functionaldataprocessing/PrintElementsOfStream.java
@@ -0,0 +1,41 @@
+package functionalstreams.functionaldataprocessing;
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.stream.Stream;
+
+/**
+ * Implement a method that prints out each element from a given stream of numbers except the first two elements.
+ *
+ * Please, use Java Stream API to solve the problem.
+ * Report a typo
+ *
+ * Sample Input 1:
+ *
+ * 1 1 3 5 7 9
+ *
+ * Sample Output 1:
+ *
+ * 3
+ * 5
+ * 7
+ * 9
+ */
+public class PrintElementsOfStream {
+    /**
+     * Prints each element from a given stream except first 2 elements.
+     *
+     * @param stream the input stream of integers
+     */
+    public static void printStream(Stream<Integer> stream) {
+        // write your code here
+        stream.skip(2).forEach(System.out::println);
+    }
+
+    // Don't change the code below
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        printStream(Arrays.stream(scanner.nextLine().split("\\s+")).map(Integer::parseInt));
+    }
+
+}
